5022de4088482828bb474f68e3b79eb1ac423344
janchou <297963025@qq.com>
2017-03-24 11:23:49 +0800

    .

---
+++ JavaBase/src/cn/java/note/javacc/Test.java
@@ -0,0 +1,26 @@
+package cn.java.note.javacc;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+
+		/*
+			javaCC 是一个能生成语法和词法分析器的生成程序。
+				https://java.net/projects/javacc/
+				http://blog.csdn.net/bhq2010/article/details/8763920
+				
+				https://javacc.org/download
+				https://www.oschina.net/question/5189_8814
+			
+			javaCC的模板文件 foo.jjt
+			foo.jjt的文件结构
+			
+			
+			生成语法和词法分析器
+			javacc foo.jj
+				
+		 */
+	}
+
+}
