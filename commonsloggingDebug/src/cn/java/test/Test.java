d36d79e0c19a2472948b1762110881c57d1d73c7
janchou <297963025@qq.com>
2017-03-24 19:52:00 +0800

    .

---
+++ commonsloggingDebug/src/cn/java/test/Test.java
@@ -0,0 +1,12 @@
+package cn.java.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Test {
+	private static Log log = LogFactory.getLog(Test.class);
+	public static void main(String[] args){
+		log.debug("this is debug info");
+		
+	}
+}
